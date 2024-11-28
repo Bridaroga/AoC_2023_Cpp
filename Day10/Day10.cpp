8c04268ddf63977e1e26cd768f742d2fb29fb79c
Raphal <931674+Bridaroga@users.noreply.github.com>
2024-11-27 23:05:47 -0500

    Guardar el archivo en un <map> y la localización del inicio "S".

--- Day10/Day10.cpp
+++ Day10/Day10.cpp
@@ -7,20 +7,21 @@
 typedef std::tuple<int, int> coordenadas;
 typedef std::map<coordenadas, char> mapa;
 
-// Ver si guardar cada char, linea o mapa
-mapa guardarLinea(std::string linea) {
-
-
-    return { 0, 0 };
-}
-
 int main() {
     std::ifstream loopDocument("Loop.txt");
     std::string lineaLoop{ "" };
+    coordenadas coordenadaInicial{};
     mapa mapaTubos{};
+    int fila{ 0 };
 
     while (!loopDocument.eof()) {
         std::getline(loopDocument, lineaLoop);
-        
+        for (int columna = 0; columna < lineaLoop.size(); columna++) {
+            mapaTubos.insert({ {fila, columna}, lineaLoop[columna] });
+            if (lineaLoop[columna] == 'S') {
+                coordenadaInicial = {fila, columna};
+            }
+        }
+        fila++;
     }
 }
